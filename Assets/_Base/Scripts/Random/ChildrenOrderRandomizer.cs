7370423da1cf3239f6586130fb1a389003ab2a63
Gabriel Bertasso <contact@gabrielbertasso.com>
2024-10-16 20:09:19 -0400

    Puzzle level

---
+++ Assets/_Base/Scripts/Random/ChildrenOrderRandomizer.cs
@@ -0,0 +1,43 @@
+﻿using FireRingStudio.Extensions;
+using Sirenix.OdinInspector;
+using System.Collections;
+using UnityEngine;
+
+namespace FireRingStudio.Assets._Base.Scripts.Random
+{
+    public class ChildrenOrderRandomizer : MonoBehaviour
+    {
+        [SerializeField] private bool _randomizeOnEnable;
+
+
+        private void OnEnable()
+        {
+            if (_randomizeOnEnable)
+            {
+                Randomize();
+            }
+        }
+
+        [Button]
+        public void Randomize()
+        {
+            Transform[] children = new Transform[transform.childCount];
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                children[i] = transform.GetChild(i);
+            }
+
+            children.Shuffle();
+
+            foreach (Transform child in children)
+            {
+                child.SetParent(null);
+            }
+
+            foreach (Transform child in children)
+            {
+                child.SetParent(transform);
+            }
+        }
+    }
+}
